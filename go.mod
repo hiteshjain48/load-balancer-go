de0edacb1116eff623daa6fac4167b7c7938ca7f
Hitesh Jain <hiteshjainhd@gmail.com>
2023-09-03 23:26:23 +0530

    done with overall structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hiteshjain48/load-balancer-go
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
+
+type Server interface {
+	Address() string
+	isAlive() bool
+	Serve(w http.ResponseWriter, r *http.Request)
+}
+
+type simpleServer struct {
+	addr string
+	proxy *httputil.ReverseProxy
+}
+
+func newSimpleServer(addr string) *simpleServer {
+	serverUrl, err := url.Parse(addr)
+	handleErr(err)
+
+	return &simpleServer{
+		addr: addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+
+	}
+}
+
+type LoadBalancer struct {
+	port string
+	servers []Server
+	roundRobinCount int
+}
+
+func NewLoadBalancer(port string, servers []Server) *LoadBalancer {
+	return &LoadBalancer{
+		port: port,
+		roundRobinCount: 0,
+		servers: servers,
+	}
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		os.Exit(1)
+	}
+}
